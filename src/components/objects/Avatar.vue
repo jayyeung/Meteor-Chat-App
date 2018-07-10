96ddb2c997d5b7c3f7ae16aa1e8cd94eb9669c8b
Jason Yeung <jsy9@sfu.ca>
2018-07-09 20:38:46 -0700

    add avatar

---
+++ src/components/objects/Avatar.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class='c-avatar'>
+		<img :src='src' alt='user avatar'/>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Avatar',
+		props: {
+			src: { type: String, required: true },
+		}
+	}
+</script>
