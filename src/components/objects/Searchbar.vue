44e74128b047075d4d8f1cf46f6c3d909419c88d
Jason Yeung <jsy9@sfu.ca>
2018-07-09 20:38:35 -0700

    add searchbar

---
+++ src/components/objects/Searchbar.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class='c-searchbar'>
+		<input type='text' @input='signalChange'/>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Searchbar',
+		methods: {
+			signalChange: function(e) {
+				this.$emit('input', e.target.value);
+			}
+		}
+	}
+</script>
