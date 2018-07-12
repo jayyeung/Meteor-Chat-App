47468c7d91b930fd81637a7f28928c9ae1330f81
Jason Yeung <jsy9@sfu.ca>
2018-07-11 23:52:36 -0700

    connect vuex store properly

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import Store from './store/index';
 
 Vue.config.productionTip = false
 
 new Vue({
-  store,
+  store: Store,
   render: h => h(App)
 }).$mount('#app')
