f0a535c999ec9c6e8a1fbe4314638b6e33f011fd
Jason Yeung <jsy9@sfu.ca>
2018-07-14 14:32:01 -0700

    apply store and meteor-tracker

--- client/main.js
+++ client/main.js
@@ -2,15 +2,21 @@ import { Meteor } from 'meteor/meteor';
 import '/imports/startup/client/user-setup';
 
 import Vue from 'vue';
+
 import store from  '/imports/ui/store';
+import meteorData from  '/imports/ui/store/data';
+
 import App from '/imports/ui/App.vue';
 
-import { ProfilesCollection as Profiles } from 'meteor/socialize:user-profile';
+import { ProfilesCollection } from 'meteor/socialize:user-profile';
 
 Meteor.startup(() => {
+	window.Profiles = ProfilesCollection;
+
 	new Vue({
 		el: '#app',
-		store,
 		...App,
+		...meteorData,
+		store,
 	});
 });
