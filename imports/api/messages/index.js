69cd1fbe670e7fd31486ee9bf5b3cea6e3dddc6e
Jason Yeung <jsy9@sfu.ca>
2018-07-15 13:45:19 -0700

    edit file

--- imports/api/messages/index.js
+++ imports/api/messages/index.js
@@ -1,3 +1,13 @@
+import { Meteor } from 'meteor/meteor';
 import { Messages } from './messages';
+
+if (Meteor.isServer) {
+	Meteor.startup(() => {
+		// remove messages after certain time (1 hour)
+		Messages._ensureIndex({created_at: 1}, {expireAfterSeconds: 6000});
+		Messages.remove({});
+	});
+}
+
 export { Messages };
 
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -6,6 +6,5 @@ import './temp-login';
 import { Messages } from '/imports/api/messages';
 
 Meteor.startup(() => {
-	Messages.remove({});
 	Meteor.users.remove({});
 });
