bb85442c56d8112bffbf9d4d850e40c53a19a678
Jason Yeung <jsy9@sfu.ca>
2018-07-22 00:12:50 -0700

    make users leave periodically

--- imports/startup/server/fake-users.js
+++ imports/startup/server/fake-users.js
@@ -3,28 +3,22 @@ import { Random } from 'meteor/random';
 import { ProfilesCollection as Profiles } from 'meteor/socialize:user-profile';
 
 const maxBots = 5;
-const delayIn = 30000;
-const delayOut = 300000;
+const delayIn = 300000;
+const delayOut = 600000;
 
 // Generate fake users that will enter
 // and leave periodically
 const generateBot = () => {
-	const fakeId = Random.id();
-	Profiles.insert({ _id: fakeId });
-	/*
 	const logIn = Math.random() * (delayOut - delayIn) + delayIn;
 	const logOut = Math.random() * (delayOut - delayIn) + delayIn;
 
-	Meteor.setTimeout(() => {
-		const fakeId = Random.id();
-		Profiles.insert({ _id: fakeId });
+	const fakeId = Random.id();
+	Profiles.insert({ _id: fakeId });
 
-		Meteor.setTimeout(() => {
-			Profiles.remove(fakeId);
-			generateBot();
-		}, delayOut);
-	}, delayIn);
-	*/
+	Meteor.setTimeout(() => {
+		Profiles.remove(fakeId);
+		Meteor.setTimeout(generateBot, logIn);
+	}, logOut);
 };
 
 for (let i = 0; i < maxBots; i++)
