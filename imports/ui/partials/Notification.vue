b99697c680466d9116a9098a2fe347899c384d01
Jason Yeung <jsy9@sfu.ca>
2018-07-21 21:20:43 -0700

    add timestamp to notifications

--- imports/ui/partials/Notification.vue
+++ imports/ui/partials/Notification.vue
@@ -1,5 +1,26 @@
 <template>
 	<div class='c-chatlist__notification'>
-		<span><slot></slot></span>
+		<span>
+			{{ date && `${getNotifyTime} &ndash; ` || '' }}
+			<slot></slot>
+		</span>
 	</div>
 </template>
+
+<script>
+export default {
+	props: {
+		date: Date
+	},
+	computed: {
+		getNotifyTime: function() {
+			const options = {
+				hour: 'numeric',
+				minute: 'numeric',
+				hour12: true
+			};
+			return (this.date).toLocaleString('en-US', options);
+		}
+	}
+}
+</script>
